9214c3a524a8aa94a5a0fea822161c904af1e7fb
Makiah Bennett <makiahtbennett@gmail.com>
2023-07-19 22:47:34 -0500

    Simplify again:

--- src/main.py
+++ src/main.py
@@ -1,49 +1,11 @@
-from transcription import record_and_transcribe
-import os
-import json
-
-def load_config_with_defaults():
-    default_config = {
-        'use_api': True,
-        'api_options': {
-            'model': 'whisper-1',
-            'language': None,
-            'temperature': 0.0,
-            'initial_prompt': None
-        },
-        'local_model_options': {
-            'model': 'base',
-            'device': None,
-            'language': None,
-            'temperature': 0.0,
-            'initial_prompt': None,
-            'condition_on_previous_text': True,
-            'verbose': False
-        },
-        'activation_key': 'ctrl+alt+space',
-        'silence_duration': 900,
-        'writing_key_press_delay': 0.008,
-        'remove_trailing_period': True,
-        'add_trailing_space': False,
-        'remove_capitalization': False,
-        'print_to_terminal': True,
-    }
-
-    config_path = os.path.join('config.json')
-    if os.path.isfile(config_path):
-        with open(config_path, 'r') as config_file:
-            user_config = json.load(config_file)
-            for key, value in user_config.items():
-                if key in default_config and value is not None:
-                    default_config[key] = value
-
-    return default_config
+from transcription import TranscriptionHandler
+import whisper
+from computercontroller import ComputerController
 
 def run():
-    config = load_config_with_defaults()
-    method = 'OpenAI\'s API' if config['use_api'] else 'a local model'
-    print(f"Listening using {method}")
-    record_and_transcribe(config)
+    TranscriptionHandler(\
+        whisper.load_model(name="base.en", device="cuda"), \
+        ComputerController()).record_and_transcribe()
 
 if __name__ == "__main__":
     run()
--- src/transcription.py
+++ src/transcription.py
@@ -5,111 +5,111 @@ import sounddevice as sd
 import tempfile
 import wave
 import webrtcvad
-import whisper
 import traceback
 from computercontroller import ComputerController
 
-def process_transcription(transcription, controller: ComputerController):
-    if transcription.endswith('.'):
-        transcription = transcription[:-1]
-    transcription = transcription.lower()
-    controller.process(transcription)
+class TranscriptionHandler:
+    def __init__(self, model, controller: ComputerController):
+        self.model = model
+        self.controller = controller
 
-def transcribe_recording(sample_rate, config, recording, controller, model):
-    audio_data = np.array(recording, dtype=np.int16)
-    print('Recording finished. Size:', audio_data.size) if config['print_to_terminal'] else ''
-    
-    # Save the recorded audio as a temporary WAV file on disk
-    with tempfile.NamedTemporaryFile(suffix='.wav', delete=False) as temp_audio_file:
-        with wave.open(temp_audio_file.name, 'wb') as wf:
-            wf.setnchannels(1)
-            wf.setsampwidth(2)  # 2 bytes (16 bits) per sample
-            wf.setframerate(sample_rate)
-            wf.writeframes(audio_data.tobytes())
+    def process_transcription(self, transcription):
+        if transcription.endswith('.'):
+            transcription = transcription[:-1]
+        transcription = transcription.lower()
+        self.controller.process(transcription)
 
-    print('Transcribing audio file...') if config['print_to_terminal'] else ''
+    def transcribe_recording(self, sample_rate, recording):
+        audio_data = np.array(recording, dtype=np.int16)
+        print('Recording finished. Size:', audio_data.size)
+        
+        # Save the recorded audio as a temporary WAV file on disk
+        with tempfile.NamedTemporaryFile(suffix='.wav', delete=False) as temp_audio_file:
+            with wave.open(temp_audio_file.name, 'wb') as wf:
+                wf.setnchannels(1)
+                wf.setsampwidth(2)  # 2 bytes (16 bits) per sample
+                wf.setframerate(sample_rate)
+                wf.writeframes(audio_data.tobytes())
 
-    response = model.transcribe(audio=temp_audio_file.name,
-                                language=None,
-                                verbose=True,
-                                initial_prompt=None,
-                                condition_on_previous_text=False,
-                                temperature=0.0,)
-    
+        print('Transcribing audio file...')
 
-    # Remove the temporary audio file
-    os.remove(temp_audio_file.name)
+        response = self.model.transcribe(audio=temp_audio_file.name,
+                                    language=None,
+                                    verbose=True,
+                                    initial_prompt=None,
+                                    condition_on_previous_text=False,
+                                    temperature=0.0,)
+        
 
-    result = response.get('text')
-    print('Transcription:', result) if config['print_to_terminal'] else ''
-    
-    process_transcription(result.strip(), controller) if result else ''
+        # Remove the temporary audio file
+        os.remove(temp_audio_file.name)
 
-"""
-Record audio from the microphone and transcribe it using the OpenAI API.
-Recording stops when the user stops speaking.
-"""
-def record_and_transcribe(config):
-    model = whisper.load_model(name="base.en", device="cuda")
+        result = response.get('text')
+        print('Transcription:', result)
+        
+        self.process_transcription(result.strip()) if result else ''
 
-    controller = ComputerController()
+    """
+    Record audio from the microphone and transcribe it using the OpenAI API.
+    Recording stops when the user stops speaking.
+    """
+    def record_and_transcribe(self):
+        sample_rate = 16000
+        frame_duration = 30  # 30ms, supported values: 10, 20, 30
+        done_speaking_silence_duration = 300  # ms
+        acceptable_mid_speaking_pause_duration = 150
+        minimum_speaking_duration = 400
 
-    sample_rate = 16000
-    frame_duration = 30  # 30ms, supported values: 10, 20, 30
-    done_speaking_silence_duration = config['silence_duration'] if config else 300  # 900ms
-    acceptable_mid_speaking_pause_duration = 150
-    minimum_speaking_duration = 400
+        def to_frames(duration):
+            return duration // frame_duration
 
-    def to_frames(duration):
-        return duration // frame_duration
+        vad = webrtcvad.Vad(3)  # Aggressiveness mode: 3 (highest)
+        buffer = []
+        recording = []
+        num_speech_not_detected_frames = 0
+        speaking_frames = 0
+        done_speaking_silence_frames = to_frames(done_speaking_silence_duration)
+        acceptable_mid_speaking_pause_frames = to_frames(acceptable_mid_speaking_pause_duration)
+        minimum_speaking_frames = to_frames(minimum_speaking_duration)
 
-    vad = webrtcvad.Vad(3)  # Aggressiveness mode: 3 (highest)
-    buffer = []
-    recording = []
-    num_speech_not_detected_frames = 0
-    speaking_frames = 0
-    done_speaking_silence_frames = to_frames(done_speaking_silence_duration)
-    acceptable_mid_speaking_pause_frames = to_frames(acceptable_mid_speaking_pause_duration)
-    minimum_speaking_frames = to_frames(minimum_speaking_duration)
-
-    try:
-        print('Recording...') if config['print_to_terminal'] else ''
-        with sd.InputStream(samplerate=sample_rate, channels=1, dtype='int16', blocksize=sample_rate * frame_duration // 1000,
-                            callback=lambda indata, frames, time, status: buffer.extend(indata[:, 0])): # https://blog.furas.pl/python-what-means-0-and-1-in-numpy-or-pandas-gb.html
-            while True:
-                # print("Running")
-                if len(buffer) < sample_rate * frame_duration // 1000:
-                    continue
-
-                # buffer only contains unprocessed frames
-                frame = buffer[:sample_rate * frame_duration // 1000]
-                buffer = buffer[sample_rate * frame_duration // 1000:]
-
-                if vad.is_speech(np.array(frame).tobytes(), sample_rate):
-                    print("Speech detected!")
-                    num_speech_not_detected_frames = 0
-                    speaking_frames += 1
-                    recording.extend(frame)
-                else:
-                    if len(recording) == 0:
+        try:
+            print('Recording...')
+            with sd.InputStream(samplerate=sample_rate, channels=1, dtype='int16', blocksize=sample_rate * frame_duration // 1000,
+                                callback=lambda indata, frames, time, status: buffer.extend(indata[:, 0])): # https://blog.furas.pl/python-what-means-0-and-1-in-numpy-or-pandas-gb.html
+                while True:
+                    # print("Running")
+                    if len(buffer) < sample_rate * frame_duration // 1000:
                         continue
 
-                    num_speech_not_detected_frames += 1
+                    # buffer only contains unprocessed frames
+                    frame = buffer[:sample_rate * frame_duration // 1000]
+                    buffer = buffer[sample_rate * frame_duration // 1000:]
 
-                    if num_speech_not_detected_frames < acceptable_mid_speaking_pause_frames:
-                        print(f"Paused: frames = {num_speech_not_detected_frames} will stop at {acceptable_mid_speaking_pause_frames}")
-                        continue
+                    if vad.is_speech(np.array(frame).tobytes(), sample_rate):
+                        print("Speech detected!")
+                        num_speech_not_detected_frames = 0
+                        speaking_frames += 1
+                        recording.extend(frame)
+                    else:
+                        if len(recording) == 0:
+                            continue
 
-                    if speaking_frames < minimum_speaking_frames:
-                        print(f"Not enough speaking time (got {speaking_frames}, needed {minimum_speaking_frames}), ignoring that last bit")
-                        recording = []
-                        speaking_frames = 0
-                    elif num_speech_not_detected_frames >= done_speaking_silence_frames:
-                        print(f"Long enough, length is {speaking_frames} required is {minimum_speaking_frames}")
-                        transcribe_recording(sample_rate, config, recording, controller, model)
-                        recording = []
-                        speaking_frames = 0
+                        num_speech_not_detected_frames += 1
 
-    except Exception as e:
-        traceback.print_exc()
+                        if num_speech_not_detected_frames < acceptable_mid_speaking_pause_frames:
+                            print(f"Paused: frames = {num_speech_not_detected_frames} will stop at {acceptable_mid_speaking_pause_frames}")
+                            continue
+
+                        if speaking_frames < minimum_speaking_frames:
+                            print(f"Not enough speaking time (got {speaking_frames}, needed {minimum_speaking_frames}), ignoring that last bit")
+                            recording = []
+                            speaking_frames = 0
+                        elif num_speech_not_detected_frames >= done_speaking_silence_frames:
+                            print(f"Long enough, length is {speaking_frames} required is {minimum_speaking_frames}")
+                            self.transcribe_recording(sample_rate, recording)
+                            recording = []
+                            speaking_frames = 0
+
+        except Exception as e:
+            traceback.print_exc()
 
